b9bb6cebe1af49d6635908e5a3450b06d5743e2e
Agacy junior <juniormartinssom@gmail.com>
2023-01-19 14:06:46 -0300

    Creating a cart tag

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -6,3 +6,7 @@ export function tagUserInfoCreate() {
     'user_email': 'jrxr@email.com'
   });
 }
+
+export function tagCartUpdate(itemsCount: string) {
+  OneSignal.sendTag('cart_items_count', itemsCount)
+}
